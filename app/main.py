324a279f69446c4d59c03bc0aeac553940117dab
C Abhinav Dev <abhinav200513@gmail.com>
2025-04-11 16:18:03 +0530

    credientials false

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # or ["*"] for all origins
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
